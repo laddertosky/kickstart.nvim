a6563385e44c62d53c289223295b4d2727683b36
jacky.chen <jacky.chen@beseye.com>
2024-05-23 11:53:50 +0800

    set indent with 4 space

--- lua/jacky/global.lua
+++ lua/jacky/global.lua
@@ -72,3 +72,6 @@ vim.opt.scrolloff = 10
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
+
+-- Set the shiftwidth, which could be inserted with <C-\>> in the visual mode
+vim.o.shiftwidth = 4

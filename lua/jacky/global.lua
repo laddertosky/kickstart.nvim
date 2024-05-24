66cc78af9eb92b696dd190c0263562d12f0a3ab9
Jacky <jacky3627@gmail.com>
2024-05-25 00:23:47 +0800

    adjust delay for raspberry 5

--- lua/jacky/global.lua
+++ lua/jacky/global.lua
@@ -42,11 +42,11 @@ vim.opt.smartcase = true
 vim.opt.signcolumn = 'yes'
 
 -- Decrease update time
-vim.opt.updatetime = 100
+vim.opt.updatetime = 50
 
 -- Decrease mapped sequence wait time
 -- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
+vim.opt.timeoutlen = 100
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true

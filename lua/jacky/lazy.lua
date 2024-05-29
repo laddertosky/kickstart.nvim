c16b081f3d7ebeb36e5f0fbe7dde65c4debb82d7
jacky.chen <jacky.chen@beseye.com>
2024-05-29 02:24:47 +0000

    use default git signs

--- lua/jacky/lazy.lua
+++ lua/jacky/lazy.lua
@@ -48,13 +48,6 @@ require('lazy').setup({
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
       current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
       current_line_blame_opts = {
         virt_text = true,

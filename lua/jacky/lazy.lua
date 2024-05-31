5367cccbb23fc0f511fa34dda4cbd998fc5f77c6
jacky.chen <jacky.chen@beseye.com>
2024-05-31 11:09:31 +0800

    add vim-test

--- lua/jacky/lazy.lua
+++ lua/jacky/lazy.lua
@@ -23,6 +23,19 @@ require('lazy').setup({
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'christoomey/vim-tmux-navigator',
   {
+    'vim-test/vim-test',
+    dependencies = {
+      'preservim/vimux',
+    },
+    config = function()
+      vim.keymap.set('n', '<leader>tn', ':TestNearest<CR>', { desc = 'Execute the [N]nearest test' })
+      vim.keymap.set('n', '<leader>tf', ':TestFile<CR>', { desc = 'Execute the tests in current [F]file' })
+      vim.keymap.set('n', '<leader>ts', ':TestSuite<CR>', { desc = 'Execute the tests in current [S]suite' })
+      vim.keymap.set('n', '<leader>tl', ':TestLast<CR>', { desc = 'Execute the [L]last test' })
+      vim.cmd "let test#strategy = 'vimux'"
+    end,
+  },
+  {
     'norcalli/nvim-terminal.lua',
     config = function()
       require('terminal').setup()

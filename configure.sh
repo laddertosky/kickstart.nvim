f6776b987661f32adb6dac9f7ca54b2466f2e8a2
laddertosky <jacky3627@gmail.com>
2024-09-28 17:12:40 +0000

    add dependencies

--- configure.sh
+++ configure.sh
@@ -2,16 +2,23 @@
 
 CURRENT_DIR=$(dirname $0)
 
+Pack=$(which apt)
+if [ -z "$Pack" ]; then
+    echo "Configure the command for package manager in this script."
+    exit 1
+fi
+Pack="sudo apt install -y"
+
+$Pack nvim \
+    jq \
+    fzf \
+    unzip \
+    npm \
+    tmux
+
 cp $CURRENT_DIR/tmux/.tmux.conf ~/
 echo "~/.tmux.conf" is updated with the file in this repo.
 
-Tmux=$(which tmux)
-
-if [ -z "$Tmux" ]; then
-    echo "Remember to install tmux with your package manager or build from the source."
-    echo "For further information, see https://github.com/tmux/tmux/wiki/Installing"
-    echo 1
-fi
-
 # install the tpm if not installed yet
 [ -d ~/.tmux ] || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "Remember to install tpm plugin with <leader>I in tmux session"

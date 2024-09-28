5d9f51cdefc896d66e0977f12c3d842c5d332662
laddertosky <jacky3627@gmail.com>
2024-09-28 23:03:47 +0000

    user space instead of tab

--- lua/jacky/global.lua
+++ lua/jacky/global.lua
@@ -76,6 +76,9 @@ vim.opt.hlsearch = true
 -- Set the shiftwidth, which could be inserted with <C-\>> in the visual mode
 vim.o.shiftwidth = 4
 
+-- Use space when user types tab
+vim.o.expandtab = true
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 

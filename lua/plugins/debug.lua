c6aef2207191a75b36d06d446cdbc05bb2c1f8ea
laddertosky <jacky3627@gmail.com>
2024-10-01 05:53:09 +0000

    modify dap keymap

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -56,8 +56,10 @@ return {
     vim.keymap.set('n', '<leader>di', dap.step_into, { desc = 'Debug: Step Into' })
     vim.keymap.set('n', '<leader>dn', dap.step_over, { desc = 'Debug: Step Over' })
     vim.keymap.set('n', '<leader>do', dap.step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<leader>dt', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-    vim.keymap.set('n', '<leader>dB', function()
+    vim.keymap.set('n', '<leader>dr', dap.restart, { desc = 'Debug: Restart' })
+    vim.keymap.set('n', '<leader>df', dap.terminate, { desc = 'Debug: Stop' })
+    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+    vim.keymap.set('n', '<leader>B', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
     end, { desc = 'Debug: Set Breakpoint' })
 

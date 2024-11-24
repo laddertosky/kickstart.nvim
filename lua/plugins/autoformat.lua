f7137d2802e0db66c4aaab92682bd11b836116f5
jacky <jacky3627@gmail.com>
2024-11-23 16:24:29 -0800

    update c,c++ formatter config

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -7,7 +7,7 @@ return {
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = {}
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -17,6 +17,8 @@ return {
         lua = { 'stylua' },
         bash = { 'shfmt' },
         sh = { 'shfmt' },
+        cpp = { 'clang-format' },
+        c = { 'clang-format' },
         --
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },

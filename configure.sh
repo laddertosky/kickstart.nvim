4901ab9c0f37a3eb386826676afb11862ec187d6
jacky <jacky3627@gmail.com>
2024-12-05 23:32:45 -0800

    add more dependencies in this script

--- configure.sh
+++ configure.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
-
 CURRENT_DIR=$(dirname $0)
+set -x
 
-Pack=$(which apt)
-if [ -z "$Pack" ]; then
-    echo "Configure the command for package manager in this script."
-    exit 1
-fi
+# Modify if use different package manager
 Pack="sudo apt install -y"
 
-$Pack neovim \
-    jq \
-    fzf \
-    ripgrep \
-    unzip \
-    npm \
-    tmux
+# Install Nvim dependencies
+$Pack ninja-build gettext cmake unzip curl build-essential
 
+NVIM_BASE="/home/$(whoami)/Documents"
+NVIM_DIR="$NVIM_BASE/neovim"
+NVIM_VERSION="v0.10.2"
+
+if [ ! -d $NVIM_DIR ]; then
+    pushd $NVIM_BASE
+
+    NVIM="https://github.com/neovim/neovim"
+    git clone $NVIM $NVIM_DIR
+    git checkout $NVIM_VERSION
+    cd $NVIM_DIR
+    make
+    sudo make install
+
+    popd
+fi
+
+# Install dependencies for some plugins
+$Pack jq fzf bat ripgrep npm tmux golang cargo
+
+# Useful utility to calculate statistics about a codebase
+TOKEI=$(which tokei)
+if [ -z "$TOKEI" ]; then
+    cargo install tokei # tokei is not found in apt repository
+    echo 'export PATH="/home/$(whoami)/.cargo/bin":$PATH' >>"~/.bashrc"
+    source "~/.bashrc"
+fi
+
+# Update the .tmux.conf
 cp $CURRENT_DIR/tmux/.tmux.conf ~/
 echo "~/.tmux.conf" is updated with the file in this repo.
 

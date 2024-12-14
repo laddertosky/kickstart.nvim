fc513bef0eee8c2b8501b37362d3c4435889d19c
jacky <jacky3627@gmail.com>
2024-12-13 22:30:26 -0800

    add terminal related keymap

--- lua/jacky/keymap.lua
+++ lua/jacky/keymap.lua
@@ -1,18 +1,10 @@
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]error messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]quickfix list' })
 
 vim.keymap.set('n', 'z/', function()
   vim.bo.filetype = 'terminal'
@@ -24,3 +16,11 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>', { silent = true })
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>', { silent = true })
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>', { silent = true })
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>', { silent = true })
+
+vim.keymap.set('n', '<a-s>', '<CMD>horizontal terminal<CR>', { silent = true })
+vim.keymap.set('n', '<a-v>', '<CMD>vertical terminal<CR>', { silent = true })
+
+vim.keymap.set('t', '<c-h>', '<c-\\><c-n><c-w>h<cr>', { silent = true })
+vim.keymap.set('t', '<c-j>', '<c-\\><c-n><c-w>j<cr>', { silent = true })
+vim.keymap.set('t', '<c-k>', '<c-\\><c-n><c-w>k<cr>', { silent = true })
+vim.keymap.set('t', '<c-l>', '<c-\\><c-n><c-w>l<cr>', { silent = true })
